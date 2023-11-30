fcc0ba16e54d6b19ca4416721a28eb6cd30ea49d
Emilio Gallicchio <emilio.gallicchio@gmail.com>
2023-11-30 17:34:09 -0500

    simulation file for syk set

---
+++ targets/syk/free_energy/CHEMBL3265014~CHEMBL3265025/mdlambda_atm.py
@@ -0,0 +1,209 @@
+from __future__ import print_function
+
+import openmm as mm
+from openmm.app import *
+from openmm import *
+from simtk.unit import *
+from sys import stdout
+import os, re,time, shutil, math
+from datetime import datetime
+
+from atmmetaforce import *
+
+
+# the multiple-time step integrator does not have a setTemperature() method
+def setTemperature(self, temperature):
+    self.setGlobalVariableByName('kT', MOLAR_GAS_CONSTANT_R*temperature)
+MTSLangevinIntegrator.setTemperature = setTemperature
+
+print("Started at: " + str(time.asctime()))
+start=datetime.now()
+
+jobname = 'CHEMBL3265014~CHEMBL3265025'
+
+
+displ = [1.12,2.88,57.36]
+displacement      = [  displ[i] for i in range(3) ] * angstrom
+lig1_restr_offset = [  0.       for i in range(3) ] * angstrom
+lig2_restr_offset = [  displ[i] for i in range(3) ] * angstrom
+
+lig1_atoms = [4457,4458,4459,4460,4461,4462,4463,4464,4465,4466,4467,4468,4469,4470,4471,4472,4473,4474,4475,4476,4477,4478,4479,4480,4481,4482,4483,4484,4485,4486,4487,4488,4489,4490,4491,4492,4493,4494,4495,4496,4497,4498,4499,4500,4501,4502,4503,4504,4505,4506,4507,4508]
+lig2_atoms = [4509,4510,4511,4512,4513,4514,4515,4516,4517,4518,4519,4520,4521,4522,4523,4524,4525,4526,4527,4528,4529,4530,4531,4532,4533,4534,4535,4536,4537,4538,4539,4540,4541,4542,4543,4544,4545,4546,4547,4548,4549,4550,4551,4552,4553,4554,4555,4556,4557,4558]
+#refatoms_lig1 = [3,11,26]
+refatoms_lig1 = [ 8, 13, 16 ]
+#refatoms_lig2 = [3,11,26]
+refatoms_lig2 = [ 8, 13, 16 ]
+rcpt_cm_atoms = [258,277,628,654,1386,1403,1418,1435,1445,1460,1479,1494,2205]
+restrained_atoms = [ 9,25,46,65,77,101,123,142,161,175,194,209,221,243,258,277,284,295,302,316,336,343,357,373,395,417,424,445,466,483,500,522,544,560,576,598,612,628,638,654,676,695,714,736,750,765,775,789,809,815,825,844,866,878,893,912,931,941,956,966,980,996,1013,1030,1047,1066,1078,1100,1106,1127,1146,1162,1186,1203,1222,1229,1248,1259,1274,1284,1299,1310,1334,1351,1370,1386,1403,1418,1435,1445,1460,1479,1494,1500,1519,1533,1555,1576,1595,1612,1629,1643,1667,1684,1700,1722,1734,1756,1770,1789,1808,1823,1842,1858,1875,1892,1908,1919,1936,1943,1960,1982,2003,2022,2037,2052,2063,2077,2097,2113,2130,2154,2166,2185,2195,2205,2229,2243,2259,2278,2297,2313,2327,2344,2361,2382,2392,2414,2433,2444,2456,2476,2483,2502,2513,2535,2545,2564,2588,2598,2610,2625,2639,2660,2681,2703,2713,2730,2744,2761,2768,2790,2822,2828,2844,2866,2890,2911,2929,2935,2950,2961,2980,2994,3015,3036,3058,3078,3089,3100,3122,3133,3145,3161,3185,3196,3216,3223,3239,3258,3275,3299,3314,3324,3344,3355,3376,3383,3400,3430,3436,3457,3481,3488,3505,3527,3534,3545,3560,3576,3590,3600,3617,3636,3651,3673,3680,3695,3719,3736,3743,3762,3768,3778,3785,3804,3810,3834,3849,3866,3887,3899,3918,3935,3949,3968,3979,4003,4017,4038,4050,4066,4081,4095,4127,4133,4140,4160,4170,4180,4196,4211,4230,4254,4273,4297,4311,4332,4353,4374,4386,4402,4418,4432,4447 ]
+
+# define the thermodynamic/alchemical state
+# the system is prepared at the alchemical intermediate state at lambda=0
+temperature = 298.15 * kelvin
+lmbd = 0.00
+lambda1 = lmbd
+lambda2 = lmbd
+alpha = 0.0 / kilocalorie_per_mole
+u0 = 0.0 * kilocalorie_per_mole
+w0coeff = 0.0 * kilocalorie_per_mole
+umsc =  1000.0 * kilocalorie_per_mole
+ubcore = 500.0 * kilocalorie_per_mole
+acore = 0.062500
+direction = 1
+
+# load system
+prmtop = AmberPrmtopFile("complex.prmtop")
+inpcrd = AmberInpcrdFile("complex.inpcrd")
+system = prmtop.createSystem(nonbondedMethod=PME, nonbondedCutoff=0.9*nanometer,
+                             constraints=HBonds)
+
+# load the ATM Meta Force facility.
+atm_utils = ATMMetaForceUtils(system)
+
+# Vsite restraints
+lig1_cm_atoms = lig1_atoms
+lig2_cm_atoms = lig2_atoms
+
+# force constant for Vsite CM-CM restraint
+kf = 0.0 * kilocalorie_per_mole/angstrom**2
+# radius of Vsite sphere
+r0 = 5.0 * angstrom #radius of Vsite sphere
+atm_utils.addRestraintForce(lig_cm_particles = lig1_cm_atoms,
+                            rcpt_cm_particles = rcpt_cm_atoms,
+                            kfcm = kf,
+                            tolcm = r0,
+                            offset = lig1_restr_offset)
+
+atm_utils.addRestraintForce(lig_cm_particles = lig2_cm_atoms,
+                            rcpt_cm_particles = rcpt_cm_atoms,
+                            kfcm = kf,
+                            tolcm = r0,
+                            offset = lig2_restr_offset)
+
+# alignment restraint
+
+lig1_ref_atoms  = [ refatoms_lig1[i]+lig1_atoms[0] for i in range(3)]
+lig2_ref_atoms  = [ refatoms_lig2[i]+lig2_atoms[0] for i in range(3)]
+atm_utils.addAlignmentForce(liga_ref_particles = lig1_ref_atoms,
+                            ligb_ref_particles = lig2_ref_atoms,
+                            kfdispl = 2.5 * kilocalorie_per_mole/angstrom**2,
+                            ktheta =  50.0 * kilocalorie_per_mole,
+                            kpsi =  50.0 * kilocalorie_per_mole,
+                            offset = lig2_restr_offset)
+
+# restrain the positions of specified atoms
+fc = 25.0 * kilocalorie_per_mole/angstrom**2
+tol = 1.5 * angstrom
+posrestr_atoms = []
+last_not_solvent_atom = lig2_atoms[len(lig2_atoms)-1]
+for at in prmtop.topology.atoms():
+    if at.index <= last_not_solvent_atom:
+        posrestr_atoms.append(at.index)
+atm_utils.addPosRestraints(posrestr_atoms, inpcrd.positions, fc, tol)
+
+# create ATM Force (direction is 1 by default)
+atmforcegroup = 2
+nonbonded_force_group = 1
+atm_utils.setNonbondedForceGroup(nonbonded_force_group)
+atmvariableforcegroups = [nonbonded_force_group]
+atmforce = ATMMetaForce(
+    lambda1, lambda2,  alpha * kilojoules_per_mole, u0/kilojoules_per_mole,
+     w0coeff/kilojoules_per_mole, umsc/kilojoules_per_mole,
+      ubcore/kilojoules_per_mole, acore, direction, atmvariableforcegroups )
+# adds all atoms to the force with zero displacement
+for at in prmtop.topology.atoms():
+    atmforce.addParticle(at.index, 0., 0., 0.)
+# the ligand atoms get displaced, ligand 1 from binding site to the solvent bulk
+# and ligand 2 from the bulk solvent to the binding site
+for i in lig1_atoms:
+    atmforce.setParticleParameters(i, i,
+    displ[0] * angstrom, displ[1] * angstrom, displ[2] * angstrom)
+for i in lig2_atoms:
+    atmforce.setParticleParameters(i, i,
+    -displ[0] * angstrom, -displ[1] * angstrom, -displ[2] * angstrom)
+
+atmforce.setForceGroup(atmforcegroup)
+system.addForce(atmforce)
+
+# Set up Langevin integrator
+frictionCoeff = 0.5 / picosecond
+MDstepsize = 0.001 * picosecond
+barostat = MonteCarloBarostat(1*bar, temperature)
+barostat.setFrequency(900000000)#disabled
+system.addForce(barostat)
+integrator = MTSLangevinIntegrator(temperature, frictionCoeff, MDstepsize, [(0,1), (atmforcegroup,1)])
+integrator.setConstraintTolerance(0.00001)
+
+# platform_name = 'OpenCL'
+platform_name = 'CUDA'
+platform = Platform.getPlatformByName(platform_name)
+properties = {}
+properties["Precision"] = "mixed"
+
+simulation = Simulation(prmtop.topology, system, integrator,platform, properties)
+print ("Using platform %s" % simulation.context.getPlatform().getName())
+simulation.context.setPositions(inpcrd.positions)
+if inpcrd.boxVectors is not None:
+    simulation.context.setPeriodicBoxVectors(*inpcrd.boxVectors)
+
+state = simulation.context.getState(getEnergy = True, groups = {0,atmforcegroup})
+pote = state.getPotentialEnergy()
+
+print( "LoadState equil.xml")
+simulation.loadState('equil.xml')
+
+#override ATM parameters
+simulation.context.setParameter(atmforce.Lambda1(), lambda1)
+simulation.context.setParameter(atmforce.Lambda2(), lambda2)
+simulation.context.setParameter(atmforce.Alpha(), alpha *kilojoules_per_mole)
+simulation.context.setParameter(atmforce.U0(), u0 /kilojoules_per_mole)
+simulation.context.setParameter(atmforce.W0(), w0coeff /kilojoules_per_mole)
+simulation.context.setParameter(atmforce.Umax(), umsc /kilojoules_per_mole)
+simulation.context.setParameter(atmforce.Ubcore(), ubcore /kilojoules_per_mole)
+simulation.context.setParameter(atmforce.Acore(), acore)
+simulation.context.setParameter(atmforce.Direction(), direction)
+
+print("Potential energy =",
+ simulation.context.getState(getEnergy = True, groups = {0,atmforcegroup}).getPotentialEnergy())
+
+totalSteps = 250000
+steps_per_cycle = 1000
+number_of_cycles = int(totalSteps/steps_per_cycle)
+deltalambda = (0.5 - 0.0)/float(number_of_cycles)
+
+simulation.reporters.append(
+    StateDataReporter(stdout, steps_per_cycle,
+     step=True, potentialEnergy = True, temperature=True, volume=True))
+# binding energy values and other parameters are recorded in this file
+with open("mdlambda.out", 'w') as fh:
+    for i in range(number_of_cycles):
+        simulation.step(steps_per_cycle)
+        state = simulation.context.getState(getEnergy = True, groups = {0,atmforcegroup})
+        pot_energy = (state.getPotentialEnergy()).value_in_unit(kilocalorie_per_mole)
+        pert_energy = (atmforce.getPerturbationEnergy(simulation.context)).value_in_unit(kilocalorie_per_mole)
+        l1 = simulation.context.getParameter(atmforce.Lambda1())
+        l2 = simulation.context.getParameter(atmforce.Lambda2())
+        a = simulation.context.getParameter(atmforce.Alpha()) / kilojoules_per_mole
+        umid = simulation.context.getParameter(atmforce.U0()) * kilojoules_per_mole
+        w0 = simulation.context.getParameter(atmforce.W0()) * kilojoules_per_mole
+        print("%f %f %f %f %f %f %f %f %f" % (temperature/kelvin,lmbd, l1, l2, a*kilocalorie_per_mole, umid/kilocalorie_per_mole, w0/kilocalorie_per_mole, pot_energy, pert_energy), file=fh )
+        fh.flush()
+        lmbd += deltalambda
+        lambda1 += deltalambda
+        lambda2 += deltalambda
+        simulation.context.setParameter(atmforce.Lambda1(), lambda1)
+        simulation.context.setParameter(atmforce.Lambda2(), lambda2)
+
+
+print( "SaveState: mdlambda.xml.")
+simulation.saveState('mdlambda.xml')
+
+positions = simulation.context.getState(getPositions=True).getPositions()
+boxsize = simulation.context.getState().getPeriodicBoxVectors()
+simulation.topology.setPeriodicBoxVectors(boxsize)
+with open('mdlambda.pdb', 'w') as output:
+  PDBFile.writeFile(simulation.topology, positions, output)
+
+end=datetime.now()
+elapsed=end - start
+print("elapsed time for equilbration ="+str(
+    elapsed.seconds+elapsed.microseconds*1e-6)+"s")
